1a38d2eb26916fc1e1c63d3e415dfc8d6b3f5476
Owais Quadri <owaisquadri01@gmail.com>
2026-07-28 21:42:03 -0400

    fix: ctrl 1/2/3/4 taken in tmux so im using ctrl + hjkl for harpoon now

--- init.lua
+++ init.lua
@@ -2009,10 +2009,10 @@ do
   vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, { desc = '[A]dd file to Harpoon' })
   vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Toggle Harpoon quick menu' })
 
-  vim.keymap.set('n', '<C-1>', function() harpoon:list():select(1) end, { desc = 'Harpoon to file 1' })
-  vim.keymap.set('n', '<C-2>', function() harpoon:list():select(2) end, { desc = 'Harpoon to file 2' })
-  vim.keymap.set('n', '<C-3>', function() harpoon:list():select(3) end, { desc = 'Harpoon to file 3' })
-  vim.keymap.set('n', '<C-4>', function() harpoon:list():select(4) end, { desc = 'Harpoon to file 4' })
+  vim.keymap.set('n', '<C-h>', function() harpoon:list():select(1) end, { desc = 'Harpoon to file 1' })
+  vim.keymap.set('n', '<C-j>', function() harpoon:list():select(2) end, { desc = 'Harpoon to file 2' })
+  vim.keymap.set('n', '<C-k>', function() harpoon:list():select(3) end, { desc = 'Harpoon to file 3' })
+  vim.keymap.set('n', '<C-l>', function() harpoon:list():select(4) end, { desc = 'Harpoon to file 4' })
 end
 
 -- ============================================================
